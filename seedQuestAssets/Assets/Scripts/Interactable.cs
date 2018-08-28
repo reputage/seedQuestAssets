4ae47b6b2930fc210188dfcc9d855939863ce252
Michael Mendoza <m.mendoza711@gmail.com>
2018-08-28 12:44:42 -0600

    Fix to merge

--- seedQuestAssets/Assets/Scripts/Interactable.cs
+++ seedQuestAssets/Assets/Scripts/Interactable.cs
@@ -10,18 +10,30 @@ public class Interactable : MonoBehaviour {
 	void Start () {
         Vector3 position = transform.position;
         position += new Vector3(0, 3.0f, 0);
+        Quaternion rotate = Quaternion.identity;
         actionSpot = Instantiate(InteractableManager.instance.actionSpotIcon, position, rotate, InteractableManager.instance.transform);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 targetPosition = Camera.main.transform.position; 
         Vector3 interactablePosition = actionSpot.transform.position;
-<<<<<<< HEAD
-        Vector3 lookAtDir = playerPosition - interactablePosition;
-=======
->>>>>>> 9d2e925ab7149bfbaf3a7e5523eb6b0d2f7bc4d0
+        Vector3 lookAtDir = targetPosition - interactablePosition;
         lookAtDir.y = 0;
 
+        Quaternion rotate =  Quaternion.LookRotation(lookAtDir);
         actionSpot.transform.rotation = rotate;
-	}
+        actionSpot.transform.Rotate(new Vector3(90.0f, 0, 0));
+	} 
+      
+    public void toggleHighlight(bool highlight) {
+        Renderer rend = transform.GetComponent<Renderer>();
+        Shader shaderDefault = Shader.Find("Standard");
         Shader shader = Shader.Find("Custom/Outline + Rim");
+
+        if (highlight)
+            rend.material.shader = shader;
+        else
+            rend.material.shader = shaderDefault;
+    } 
+} 
