9d2e925ab7149bfbaf3a7e5523eb6b0d2f7bc4d0
Michael Mendoza <m.mendoza711@gmail.com>
2018-08-28 12:13:38 -0600

    ActionSpot Icon now billboards toward camera

--- seedQuestAssets/Assets/Scripts/Interactable.cs
+++ seedQuestAssets/Assets/Scripts/Interactable.cs
@@ -10,17 +10,30 @@ public class Interactable : MonoBehaviour {
 	void Start () {
         Vector3 position = transform.position;
         position += new Vector3(0, 3.0f, 0);
-        Quaternion rotate = Quaternion.Euler(new Vector3(90.0f, 0, 0));
+        Quaternion rotate = Quaternion.identity;
         actionSpot = Instantiate(InteractableManager.instance.actionSpotIcon, position, rotate, InteractableManager.instance.transform);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 playerPosition = PlayerManager.GetPlayer().position;
+        Vector3 targetPosition = Camera.main.transform.position; 
         Vector3 interactablePosition = actionSpot.transform.position;
-        Vector3 lookAtDir = playerPosition - interactablePosition;
+        Vector3 lookAtDir = targetPosition - interactablePosition;
+        lookAtDir.y = 0;
 
-        Quaternion rotate = Quaternion.LookRotation(lookAtDir);
+        Quaternion rotate =  Quaternion.LookRotation(lookAtDir);
         actionSpot.transform.rotation = rotate;
+        actionSpot.transform.Rotate(new Vector3(90.0f, 0, 0));
 	}
-}
+
+    public void toggleHighlight(bool highlight) {
+        Renderer rend = transform.GetComponent<Renderer>();
+        Shader shaderDefault = Shader.Find("Standard");
+        Shader shader = Shader.Find("Custom/Outline + Rim");
+
+        if (highlight)
+            rend.material.shader = shader;
+        else
+            rend.material.shader = shaderDefault;
+    } 
+} 
--- seedQuestAssets/Assets/Scripts/InteractableManager.cs
+++ seedQuestAssets/Assets/Scripts/InteractableManager.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class InteractableManager : MonoBehaviour {
 
     static public InteractableManager instance = null;
+    public float interactDistance = 2.0f;
     public GameObject actionSpotIcon;
 
 	// Use this for initialization
 	void Awake () {
         instance = this;
 	}
-	
+
+    private void Update() {
+        Interactable[] list = FindObjectsOfType<Interactable>();
+
+        foreach(Interactable item in list) {
+            Vector3 playerPosition = PlayerManager.GetPlayer().position;
+            float dist = (item.transform.position - playerPosition).magnitude;
+            if (dist < interactDistance)
+                item.toggleHighlight(true);
+            else
+                item.toggleHighlight(false);
+        }
+    }
+
 }
