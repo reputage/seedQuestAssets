37307bd65faeb4fb9183dd54abb18ccc162e9600
Michael Mendoza <m.mendoza711@gmail.com>
2018-09-19 12:45:43 -0600

    Refactored Folders. Added Farm to Demo. Added IslandWorld Prefab

---
+++ seedQuestAssets/Assets/SeedQuestAssets/Scripts/SettingsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour {
+
+    static bool useDebug = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+	}
+}
