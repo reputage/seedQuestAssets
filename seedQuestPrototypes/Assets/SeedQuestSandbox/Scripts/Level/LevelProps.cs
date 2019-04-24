40e8a1d70927e04021f72886123d08d47f72a25c
Michael Mendoza <m.mendoza711@gmail.com>
2019-04-24 14:31:53 -0600

    Updated StartScreen for dynamically changing panel information

---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Level/LevelProps.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelProps {
+    public string name;
+    public string scenename;
+    public Sprite preview;
+}
