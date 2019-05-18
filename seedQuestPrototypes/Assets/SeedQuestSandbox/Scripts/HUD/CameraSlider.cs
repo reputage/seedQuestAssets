d97a8606d2281ad0313ed8d236dde3461b96e84c
Brady Hammond <bradymh23@gmail.com>
2019-05-18 09:48:25 -0600

    Add Scrollwheel and Graduated Rehearsal

--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/HUD/CameraSlider.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/HUD/CameraSlider.cs
@@ -12,6 +12,66 @@ public class CameraSlider : MonoBehaviour
         slider.onValueChanged.AddListener(delegate {ValueChangeCheck(); });
     }
 
+    public void Update()
+    {
+        ListenForKeyDown();
+    }
+
+    public void ListenForKeyDown()
+    {
+        var d = Input.GetAxis("Mouse ScrollWheel");
+        if (d > 0f)
+        {
+            if (IsometricCamera.StaticDistance < 40)
+            {
+                if (IsometricCamera.StaticDistance + d > 40)
+                {
+                    IsometricCamera.StaticDistance = 40;
+                    slider.value = 40;
+                }
+                else
+                {
+                    IsometricCamera.StaticDistance += d;
+                    slider.value += d;
+                }
+            }
+        }
+        else if (d < 0f)
+        {
+            if (IsometricCamera.StaticDistance > 5)
+            {
+                if (IsometricCamera.StaticDistance + d < 5)
+                {
+                    IsometricCamera.StaticDistance = 5;
+                    slider.value = 5;
+                }
+                else
+                {
+                    IsometricCamera.StaticDistance += d;
+                    slider.value += d;
+                }
+            }
+        }
+    }
+
+   public void ZoomOut()
+    {
+        if (IsometricCamera.StaticDistance < 40)
+        {
+            IsometricCamera.StaticDistance += 1;
+            slider.value += 1;
+        }
+    }
+
+    public void ZoomIn()
+    {
+        if (IsometricCamera.StaticDistance > 5)
+        {
+            IsometricCamera.StaticDistance -= 1;
+            slider.value -= 1;
+        }
+    }
+
     public void ValueChangeCheck()
     {
         IsometricCamera.StaticDistance = slider.value;
