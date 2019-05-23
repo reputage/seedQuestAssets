a091a5fed7d3b835c0dffdae580cb5d4f2475126
Michael Mendoza <m.mendoza711@gmail.com>
2019-05-22 20:06:39 -0600

    Added volume controls to pause menu

---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Prefabs/HUD/VolumeControlsUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControlsUI : MonoBehaviour
+{
+    private Slider volSlider;
+
+    // Start is called before the first frame update
+    void Start() {
+        volSlider = GetComponentInChildren<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        SettingsManager.MasterVolume = volSlider.value;
+    }
+}
