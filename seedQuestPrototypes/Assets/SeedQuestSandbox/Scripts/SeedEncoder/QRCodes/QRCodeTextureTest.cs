3ddc8cb53579405fd78ca5d817d4a35fb9b91172
Weston Bodily <westonbodily7z@gmail.com>
2019-06-26 09:58:55 -0600

    Added QR code generating functionality

---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/SeedEncoder/QRCodes/QRCodeTextureTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using QRCoder;
+using QRCoder.Unity;
+
+public class QRCodeTextureTest : MonoBehaviour
+{
+
+    public RawImage rawImage;
+
+    void Start()
+    {
+        rawImage = GetComponent<RawImage>();
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode("Hello world!", QRCodeGenerator.ECCLevel.Q);
+        UnityQRCode qrCode = new UnityQRCode(qrCodeData);
+        Texture2D qrCodeAsTexture2D = qrCode.GetGraphic(20);
+        rawImage.texture = qrCodeAsTexture2D;
+    }
+
+
+}
