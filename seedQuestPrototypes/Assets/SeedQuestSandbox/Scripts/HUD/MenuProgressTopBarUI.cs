8ed5c52f4e2ab3ed58d1de52eb6373bc4ba3149d
Michael Mendoza <m.mendoza711@gmail.com>
2019-07-10 12:19:49 -0600

    Updated HUD. Created MenuProgressTopBar, HelpMenu, and new PauseMenu

---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/HUD/MenuProgressTopBarUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuProgressTopBarUI : MonoBehaviour
+{
+
+    public void OnClickMenuButton()
+    {
+        PauseMenuUI.ToggleOn();
+    }
+
+    public void OnClickHelpButton()
+    {
+        HelpMenuUI.ToggleOn();
+    }
+
+    public void OnClickUndoButton()
+    {
+
+    }
+
+}
---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/HUD/PauseMenuUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    static private PauseMenuUI instance = null;
+    static private PauseMenuUI setInstance() { instance = HUDManager.Instance.GetComponentInChildren<PauseMenuUI>(true); return instance; }
+    static public PauseMenuUI Instance { get { return instance == null ? setInstance() : instance; } }
+
+    public void Toggle() {
+        bool active = Instance.gameObject.activeSelf;
+        Instance.gameObject.SetActive(!active);
+    }
+
+    public static void ToggleOn() {
+       bool active = Instance.gameObject.activeSelf;
+       Instance.gameObject.SetActive(true);
+    }
+}
