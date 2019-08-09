cabb4e84b4d5f9ae30fb2316a8138fba87a18192
Brady Hammond <bradymh23@gmail.com>
2019-08-09 16:22:46 -0600

    Fast Recovery Updates

--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/HUD/FastRecoveryUI.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/HUD/FastRecoveryUI.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using SeedQuest.Interactables;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class FastRecoveryUI : MonoBehaviour
@@ -14,13 +13,13 @@ public class FastRecoveryUI : MonoBehaviour
 
     public Sprite interactableIcon;
     public Sprite interactableIconSelected;
-    /*public bool useInteractableUIPositions;
-    public bool useRenderTexture;*/
+    //public bool useInteractableUIPositions;
+    public bool useRenderTexture;
 
     private Image map;
     private Image overlay;
     private List<Button> buttons;
-    private Button buttonPrefab;
+    private GameObject buttonPrefab;
     private GameObject interactableGroup;
     private Button[] interactableButtons;
     private TMPro.TMP_Text interactableTitle;
@@ -30,9 +29,9 @@ public class FastRecoveryUI : MonoBehaviour
     {
         Image[] images = gameObject.GetComponentsInChildren<Image>();
         overlay = images[0];
-        map = images[3];
-        startingTitleImage = images[6];
-        buttonPrefab = images[4].gameObject.GetComponent<Button>();
+        map = images[5];
+        startingTitleImage = images[8];
+        buttonPrefab = images[6].transform.parent.gameObject;
         interactableGroup = gameObject.transform.GetChild(0).GetChild(0).GetChild(6).GetChild(2).gameObject;
         interactableButtons = interactableGroup.GetComponentsInChildren<Button>();
         TMPro.TMP_Text[] texts = gameObject.GetComponentsInChildren<TMPro.TMP_Text>();
@@ -42,28 +41,26 @@ public class FastRecoveryUI : MonoBehaviour
         Toggle();
         ToggleInteractableGroup(false);
 
-       /* if (useRenderTexture)
+        if (useRenderTexture)
         {
             GameObject mapObject = map.gameObject;
             Destroy(map);
             mapObject.AddComponent<RawImage>();
+
         }
 
-        else
-        {*/
-            map.sprite = source;
-            map.rectTransform.sizeDelta = new Vector2(780 / source.bounds.size.y * source.bounds.size.x, 780);
-        //}
+        map.sprite = source;
+        map.rectTransform.sizeDelta = new Vector2(700 / source.bounds.size.y * source.bounds.size.x, 700);
 
         Interactable[] interactables = InteractableManager.InteractableList;
         buttons = new List<Button>();
         foreach (Interactable interactable in interactables)
         {
-            Button button = Instantiate(buttonPrefab);
-            button.transform.SetParent(map.transform);
-            //if (!useInteractableUIPositions)
-                button.transform.localPosition = new Vector3(interactable.transform.localPosition.x * scale + xOffset, interactable.transform.localPosition.z * scale + yOffset, 0);
-            button.gameObject.SetActive(true);
+            GameObject buttonObject = Instantiate(buttonPrefab);
+            Button button = buttonObject.GetComponentInChildren<Button>();
+            buttonObject.transform.SetParent(map.transform);
+            buttonObject.transform.localPosition = new Vector3(interactable.transform.localPosition.x * scale + xOffset, interactable.transform.localPosition.z * scale + yOffset, 0);
+            buttonObject.gameObject.SetActive(true);
             button.onClick.AddListener(() => OnButtonClick(interactable, button));
             buttons.Add(button);
         }
@@ -73,18 +70,6 @@ public class FastRecoveryUI : MonoBehaviour
     {
         ListenForKeyDown();
     }
-
-    /*private void LateUpdate()
-    {
-        if (useInteractableUIPositions)
-        {
-            for (int i = 0; i < InteractableManager.InteractableList.Length; i++)
-            {
-                buttons[i].transform.localPosition = new Vector3(InteractableManager.InteractableList[i].interactableUI.ActionUIRect.localPosition.x * scale + xOffset, InteractableManager.InteractableList[i].interactableUI.ActionUIRect.localPosition.y * scale + yOffset);
-            }
-            useInteractableUIPositions = false;
-        }
-    }*/
 
     private void ListenForKeyDown()
     {
@@ -103,6 +88,7 @@ public class FastRecoveryUI : MonoBehaviour
             foreach (Button button in buttons)
             {
                 button.gameObject.GetComponent<Image>().sprite = interactableIcon;
+                button.transform.parent.GetChild(1).gameObject.SetActive(false);
             }
             InteractablePreviewUI.ClearPreviewObject();
             ToggleInteractableGroup(false);
@@ -134,14 +120,24 @@ public class FastRecoveryUI : MonoBehaviour
 
     public void OnButtonClick(Interactable interactable, Button button)
     {
+        Debug.Log("Click");
         foreach (Button interactableButton in buttons)
         {
             if (interactableButton != button)
             {
                 interactableButton.gameObject.GetComponent<Image>().sprite = interactableIcon;
+                interactableButton.transform.parent.GetChild(1).gameObject.SetActive(false);
+                interactableButton.transform.parent.GetChild(0).gameObject.SetActive(true);
+            }
+
+            else
+            {
+                interactableButton.gameObject.GetComponent<Image>().sprite = interactableIconSelected;
+                interactableButton.transform.parent.GetChild(0).gameObject.SetActive(false);
+                interactableButton.transform.parent.GetChild(1).gameObject.SetActive(true);
             }
         }
-        button.gameObject.GetComponent<Image>().sprite = interactableIconSelected; 
+        //button.gameObject.GetComponent<Image>().sprite = interactableIconSelected; 
         AudioManager.Play("UI_Hover");
         if (interactableTitle.text != interactable.Name)
         {
@@ -163,6 +159,8 @@ public class FastRecoveryUI : MonoBehaviour
         else
         {
             button.gameObject.GetComponent<Image>().sprite = interactableIcon;
+            button.transform.parent.GetChild(1).gameObject.SetActive(false);
+            button.transform.parent.GetChild(0).gameObject.SetActive(true);
             //EventSystem.current.SetSelectedGameObject(null);
             InteractablePreviewUI.ClearPreviewObject();
             startingTitleImage.gameObject.SetActive(false);
