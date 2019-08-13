a879d08675c6fc31a01ba167f3a4c03f10650d7e
Brady Hammond <bradymh23@gmail.com>
2019-08-12 23:20:32 -0600

    Finish Fast Recovery Changes

---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/General UI/ImageDragger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ImageDragger : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (transform.localPosition.x > 500)
+        {
+            transform.localPosition = new Vector3(500, transform.localPosition.y, 0);
+        }
+
+        else if (transform.localPosition.x < -500)
+        {
+            transform.localPosition = new Vector3(-500, transform.localPosition.y, 0);
+        }
+
+        if (transform.localPosition.y > 600)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, 600, 0);
+        }
+
+        else if (transform.localPosition.y < -600)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, -600, 0);
+        }
+    }
+}
--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/HUD/FastRecoveryUI.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/HUD/FastRecoveryUI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using SeedQuest.Interactables;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class FastRecoveryUI : MonoBehaviour
@@ -15,8 +16,10 @@ public class FastRecoveryUI : MonoBehaviour
     public Sprite interactableIconSelected;
     //public bool useInteractableUIPositions;
     public bool useRenderTexture;
+    public float renderCameraHeight;
 
     private Image map;
+    private RawImage rawMap;
     private Image overlay;
     private List<Button> buttons;
     private GameObject buttonPrefab;
@@ -24,33 +27,52 @@ public class FastRecoveryUI : MonoBehaviour
     private Button[] interactableButtons;
     private TMPro.TMP_Text interactableTitle;
     private Image startingTitleImage;
+    private Slider slider;
 
     private void Start()
     {
         Image[] images = gameObject.GetComponentsInChildren<Image>();
         overlay = images[0];
-        map = images[5];
-        startingTitleImage = images[8];
-        buttonPrefab = images[6].transform.parent.gameObject;
+        map = images[3];
+        rawMap = gameObject.GetComponentInChildren<RawImage>();
+        startingTitleImage = images[6];
+        buttonPrefab = images[4].transform.parent.gameObject;
         interactableGroup = gameObject.transform.GetChild(0).GetChild(0).GetChild(6).GetChild(2).gameObject;
         interactableButtons = interactableGroup.GetComponentsInChildren<Button>();
         TMPro.TMP_Text[] texts = gameObject.GetComponentsInChildren<TMPro.TMP_Text>();
         interactableTitle = texts[2];
+        slider = gameObject.GetComponentInChildren<Slider>();
+        slider.onValueChanged.AddListener(delegate { OnSlideValueChanged(); });
 
         buttonPrefab.gameObject.SetActive(false);
         Toggle();
         ToggleInteractableGroup(false);
 
+        Transform buttonGroup;
         if (useRenderTexture)
         {
-            GameObject mapObject = map.gameObject;
-            Destroy(map);
-            mapObject.AddComponent<RawImage>();
-
+            map.gameObject.SetActive(false);
+            GameObject tempCameraObject = new GameObject();
+            tempCameraObject.name = "TempCamera";
+            Camera tempCamera = tempCameraObject.AddComponent<Camera>();
+            tempCamera.transform.localPosition = new Vector3(0, renderCameraHeight, 0);
+            tempCamera.transform.eulerAngles = new Vector3(90, 0, 0);
+            RenderTexture target = new RenderTexture(512, 512, 16, RenderTextureFormat.ARGB32);
+            tempCamera.targetTexture = target;
+            tempCamera.enabled = false;
+            tempCamera.Render();
+            rawMap.texture = tempCamera.targetTexture;
+            rawMap.rectTransform.sizeDelta = new Vector2(1000, 1000);
+            buttonGroup = rawMap.transform.GetChild(0);
         }
 
-        map.sprite = source;
-        map.rectTransform.sizeDelta = new Vector2(700 / source.bounds.size.y * source.bounds.size.x, 700);
+        else
+        {
+            rawMap.gameObject.SetActive(false);
+            map.sprite = source;
+            map.rectTransform.sizeDelta = new Vector2(1000 / source.bounds.size.y * source.bounds.size.x, 1000);
+            buttonGroup = map.transform.GetChild(0);
+        }
 
         Interactable[] interactables = InteractableManager.InteractableList;
         buttons = new List<Button>();
@@ -58,12 +80,14 @@ public class FastRecoveryUI : MonoBehaviour
         {
             GameObject buttonObject = Instantiate(buttonPrefab);
             Button button = buttonObject.GetComponentInChildren<Button>();
-            buttonObject.transform.SetParent(map.transform);
-            buttonObject.transform.localPosition = new Vector3(interactable.transform.localPosition.x * scale + xOffset, interactable.transform.localPosition.z * scale + yOffset, 0);
+            buttonObject.transform.SetParent(buttonGroup);
+            buttonObject.transform.localPosition = new Vector3(interactable.transform.localPosition.x * scale, interactable.transform.localPosition.z * scale, 0);
             buttonObject.gameObject.SetActive(true);
             button.onClick.AddListener(() => OnButtonClick(interactable, button));
             buttons.Add(button);
         }
+
+        buttonGroup.localPosition = new Vector3(xOffset, yOffset, 0);
     }
 
     private void Update()
@@ -97,6 +121,17 @@ public class FastRecoveryUI : MonoBehaviour
             {
                 interactableButtons[i].onClick.RemoveAllListeners();
             }
+            if (useRenderTexture)
+            {
+                rawMap.transform.localPosition = new Vector3(0, 0, 0);
+                rawMap.rectTransform.sizeDelta = new Vector2(1000, 1000);
+            }
+            else
+            {
+                map.transform.localPosition = new Vector3(0, 0, 0);
+                map.rectTransform.sizeDelta = new Vector2(1000 / source.bounds.size.y * source.bounds.size.x, 1000);
+            }
+            slider.value = 1000;
         }
         overlay.gameObject.SetActive(!active);
     }
@@ -137,7 +172,6 @@ public class FastRecoveryUI : MonoBehaviour
                 interactableButton.transform.parent.GetChild(1).gameObject.SetActive(true);
             }
         }
-        //button.gameObject.GetComponent<Image>().sprite = interactableIconSelected; 
         AudioManager.Play("UI_Hover");
         if (interactableTitle.text != interactable.Name)
         {
@@ -177,6 +211,40 @@ public class FastRecoveryUI : MonoBehaviour
     {
         AudioManager.Play("UI_Click");
         InteractablePreviewUI.SetPreviewAction(index);
+    }
+
+    public void OnSlideValueChanged()
+    {
+        InteractablePreviewUI.ClearPreviewObject();
+        startingTitleImage.gameObject.SetActive(false);
+        ToggleInteractableGroup(false);
+
+        for (int i = 0; i < 4; i++)
+        {
+            interactableButtons[i].onClick.RemoveAllListeners();
+        }
+        float newScale = (scale * slider.value) / 1000;
+        float newXOffset = (xOffset * slider.value) / 1000;
+        float newYOffset = (yOffset * slider.value) / 1000;
+
+        Interactable[] interactables = InteractableManager.InteractableList;
+        for (int i = 0; i < interactables.Length; i++)
+        {
+            buttons[i].gameObject.GetComponent<Image>().sprite = interactableIcon;
+            buttons[i].transform.parent.GetChild(1).gameObject.SetActive(false);
+            buttons[i].transform.parent.GetChild(0).gameObject.SetActive(true);
+            buttons[i].transform.parent.localPosition = new Vector3(interactables[i].transform.localPosition.x * newScale, interactables[i].transform.localPosition.z * newScale, 0);
+        }
+        if (useRenderTexture)
+        {
+            rawMap.rectTransform.sizeDelta = new Vector2(slider.value, slider.value);
+            rawMap.transform.GetChild(0).localPosition = new Vector3(newXOffset, newYOffset, 0);
+        }
+        else
+        {
+            map.rectTransform.sizeDelta = new Vector2(slider.value / source.bounds.size.y * source.bounds.size.x, slider.value);
+            map.transform.GetChild(0).localPosition = new Vector3(newXOffset, newYOffset, 0);
+        }
     }
 }
 
