314970f3b02ba16db6061c8dcc068d1d703ac37a
Michael Mendoza <m.mendoza711@gmail.com>
2019-08-22 14:15:45 -0600

    Updated MenuScreen_V2. Added ActionLineUp prefab.

--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/SceneLineUpCanvas.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/SceneLineUpCanvas.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class SceneLineUpCanvas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Continue() {
+        MenuScreenV2.Instance.GoToActionLineUp();
     }
 }
--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/SeedSetupCanvas.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/SeedSetupCanvas.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SeedSetupCanvas : MonoBehaviour
 {
+    public void Back() {
+        MenuScreenV2.Instance.GoToStart();
+    }
+
     public void FindKey() {
         MenuScreenV2.Instance.GoToEncodeSeed();
     }
