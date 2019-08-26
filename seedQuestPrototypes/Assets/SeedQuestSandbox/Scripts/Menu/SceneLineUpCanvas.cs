b01a92a96e253c9edadaba2bd8254bc9aad1ba9f
Michael Mendoza <m.mendoza711@gmail.com>
2019-08-26 10:57:05 -0600

    Setup for EncodeSeed and SceneLineUp

--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/SceneLineUpCanvas.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/SceneLineUpCanvas.cs
@@ -1,9 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SceneLineUpCanvas : MonoBehaviour
 {
+    public Image[] worldImages;
+    public Image[] worldOutlines;
+    public TextMeshProUGUI[] worldText;
+
+    void Start() {
+        foreach (Image outline in worldOutlines){
+            outline.gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            worldImages[i].sprite = WorldManager.CurrentSceneList[i].preview;
+            worldText[i].text = WorldManager.CurrentSceneList[i].name;
+        }
+    }
+
+    public void SetupLoading(int index) {
+        foreach (Image outline in worldOutlines) {
+            outline.gameObject.SetActive(false);
+        }
+        worldOutlines[index].gameObject.SetActive(true);
+    }
+
     public void Continue() {
         MenuScreenV2.Instance.GoToActionLineUp();
     }
