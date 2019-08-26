44ae79b3e6c3d27253bc14707bcab5275f0beafe
Michael Mendoza <m.mendoza711@gmail.com>
2019-08-26 14:47:53 -0600

    Added access to Tutorial from StartScreen

--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/StartScreenCanvas.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/StartScreenCanvas.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StartScreenCanvas : MonoBehaviour
 {
@@ -11,4 +13,27 @@ public class StartScreenCanvas : MonoBehaviour
     public void FindKey() {
         MenuScreenV2.Instance.SetModeRecoverSeed();
     }
+
+    public void StartTutorial(){
+        StartCoroutine(LoadAsync("NonnaISO"));
+    }
+
+    IEnumerator LoadAsync(string sceneName) {
+        yield return new WaitForSeconds(0.5f);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone) {
+
+            if (operation.progress >= 0.9f) {
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+    }
+
 }
