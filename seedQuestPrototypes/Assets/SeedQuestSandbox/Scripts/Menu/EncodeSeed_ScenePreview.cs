1376d07323fa311e1c0a7cf06b57af3977a9d9d9
Michael Mendoza <m.mendoza711@gmail.com>
2019-08-27 16:52:51 -0600

    Update to EncodeSeed

---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/EncodeSeed_ScenePreview.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class EncodeSeed_ScenePreview : MonoBehaviour
+{
+    private Image preview;
+    private Image shade;
+    private TextMeshProUGUI text;
+
+}
