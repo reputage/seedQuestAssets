64f9f7f6c88e44a871e22bed2b7343915551c38d
Michael Mendoza <m.mendoza711@gmail.com>
2019-09-10 16:32:12 -0400

    Removed unnecessary code from InteractableUI

--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Interactables/InteractableUI.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Interactables/InteractableUI.cs
@@ -8,9 +8,6 @@ namespace SeedQuest.Interactables
     [System.Serializable]
     public class InteractableUI { 
         public string name = "";
-        public int fontSize = 36;
-        public float scaleSize = 1;
-        public Vector3 rotationOffset = new Vector3(0, 0, 0);
         public Vector3 positionOffset = new Vector3(0, 0, 0);
     }
 }
