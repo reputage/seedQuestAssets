d4d5b0372d0b232372ca8fe4910c929020d6134e
Weston Bodily <westonbodily7z@gmail.com>
2019-10-17 17:00:56 -0600

    Ascii conversion scripts in progress

---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/SeedEncoder/Seed Utilities/AsciiConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using SeedQuest.SeedEncoder;
+
+public static class AsciiConverter 
+{
+    public static byte[] asciiToByte(string ascii)
+    {
+        return Encoding.ASCII.GetBytes(ascii);;
+    }
+
+    public static string byteToAscii(byte[] bytes)
+    {
+        return Encoding.ASCII.GetString(bytes);
+    }
+
+    public static string hexToAscii(string hex)
+    {
+        byte[] bytes = SeedToByte.HexStringToByteArray(hex);
+        return Encoding.ASCII.GetString(bytes);
+    }
+
+    public static string asciiToHex(string ascii)
+    {
+        byte[] bytes = Encoding.ASCII.GetBytes(ascii);
+        return SeedToByte.ByteArrayToHex(bytes);
+    }
+}
