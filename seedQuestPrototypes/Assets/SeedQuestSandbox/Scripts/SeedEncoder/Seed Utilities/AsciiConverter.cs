4e668f5c2bdf4cf6b2acdff34cfe0e3bb8bb26d6
Weston Bodily <westonbodily7z@gmail.com>
2019-10-18 11:46:04 -0600

    Fleshed out custom Ascii conversion functions

--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/SeedEncoder/Seed Utilities/AsciiConverter.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/SeedEncoder/Seed Utilities/AsciiConverter.cs
@@ -7,25 +7,44 @@ using SeedQuest.SeedEncoder;
 
 public static class AsciiConverter 
 {
+    public static char[] asciiArray = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ".ToCharArray();
+
     public static byte[] asciiToByte(string ascii)
     {
-        return Encoding.ASCII.GetBytes(ascii);;
+        byte[] bytes = new byte[ascii.Length];
+        for (int i = 0; i < ascii.Length; i++)
+        {
+            char j = ascii[i].ToChar();
+            bytes[i] = asciiArray.GetLowerBound(j);
+        }
+
+        return bytes;
     }
 
     public static string byteToAscii(byte[] bytes)
     {
-        return Encoding.ASCII.GetString(bytes);
+        string ascii = "";
+
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            if (bytes[i] > 95)
+                ascii += " ";
+            else
+                ascii += asciiArray[bytes[i]].ToString();
+        }
+
+        return ascii;
     }
 
     public static string hexToAscii(string hex)
     {
         byte[] bytes = SeedToByte.HexStringToByteArray(hex);
-        return Encoding.ASCII.GetString(bytes);
+        return byteToAscii(bytes);
     }
 
     public static string asciiToHex(string ascii)
     {
-        byte[] bytes = Encoding.ASCII.GetBytes(ascii);
+        byte[] bytes = asciiToByte(ascii);
         return SeedToByte.ByteArrayToHex(bytes);
     }
 }
