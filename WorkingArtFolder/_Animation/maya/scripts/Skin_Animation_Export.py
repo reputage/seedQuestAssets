adece8ab344af077cb6c997f218eeb92c400e400
AndyWinkler <winkanimation@gmail.com>
2019-10-25 01:47:01 -0600

    replaced maya-made ui with custom ui file. Code now displays and manages that specific UI.

--- WorkingArtFolder/_Animation/maya/scripts/Skin_Animation_Export.py
+++ WorkingArtFolder/_Animation/maya/scripts/Skin_Animation_Export.py
@@ -4,81 +4,90 @@ import os
 import os.path as path
 import sys
 
-#Import reference
-#Find base skin joint
-#Bake hierarchy animation (open dialogue for settings)
-#Remove namespace from scene
-#Delete rig from maya scene
-#Open export dialog and settings (default fbx media entertainment should work)
-    #Side note, maybe use maya->unity export
+#ui dependencies
+import shiboken2
+# Maya specific libs
+from PySide2.QtCore import *
+from PySide2.QtUiTools import *
+from PySide2.QtWidgets import *
+import maya.OpenMayaUI as MayaUI
 
-#mc.GamePipeline(sp = "Unity", sup = True)
-
-
-
-class Initialize(object):
-    global renderLayerWin
-    renderLayerWin = ''
+def get_main_window():
+    try:
+        mayaMainWindowPtr = MayaUI.MQtUtil.mainWindow()
+        mayaMainWindow = shiboken2.wrapInstance(long(mayaMainWindowPtr), QWidget)
+    except:
+        mayaMainWindow = None
+    return mayaMainWindow
     
-    def __init__(self):
-        #Set system python path (might not be needed...)
-        
-        #Get user path
-        user = path.abspath(path.join(os.getcwd()))
-        
-        self.windowName = windowName = "Consensys-Animation-Export"
-        
-        self.skinJoints = skinJoints = []
-        self.animRig = animRig = []
-        self.skinMesh = skinMesh = []
-        
-        self.icon = icon = mc.internalVar(upd = True) + "/icons/folder.png"
-        
-        #Get basic directories
-        self.imageDirectory = imageDirectory = str(mc.workspace(q=True, rd = True)) + "sourceimages"
-        self.lightsDirectory = assetsDirectory = str(mc.workspace(q=True, rd = True)) + "sourceimages"
-        self.referenceDirectory = referenceDirectory = str(mc.workspace(q=True, rd = True)) + "assets"
-        self.miscDirectory = miscDirectory = str(mc.workspace(q=True, rd = True)) + "sourceimages"
+class window_ui(QDialog):
 
-        try:
-            pm.deleteUI(self.windowName, window = True)
-        except:
-            pass
+    def __init__(self, *args, **kwargs):
+        super(window_ui, self).__init__(*args, **kwargs)
 
-        if mc.window("Consensys-Animation-Export", exists = True):
-            pm.deleteUI("Consensys-Animation-Export")
-
-        #calls function that grabs all texture references in scene and categorizes them into appropriate arrays
-        self.BuildWindow()
-
-    def BuildWindow(self):
+        #get ui path
+        path = str(os.path.expanduser('~') + "/maya/2019/scripts/ui/consensys_export.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        self.qtui = QUiLoader().load(ui_file, parent=self)
+        ui_file.close()
         
-        prepWin = mc.window(self.windowName, w = 650, h = 600, mnb = True, mxb = True, s = 1)
-        self.baseColumnLayout = baseColumnLayout = "base_ColumLayout"
-        winHeight = 400
+        self.setParent(get_main_window())
+        self.setWindowFlags(Qt.Window)
+        self.setWindowTitle('Consensys Animation Export')
+        self.setObjectName('ExportUI')
+        self.setWindowFlags(Qt.Window | Qt.WindowStaysOnTopHint)
+        self.setContentsMargins(5, 5, 5, 5)
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
 
-        if(mc.frameLayout(self.baseColumnLayout, exists=True)):
-            pm.deleteUI(self.baseColumnLayout)
-        mainLayout = mc.scrollLayout(self.baseColumnLayout, w = 650, h= winHeight)
+        self.setLayout(self.main_layout)
+        self.main_layout.addWidget(self.qtui)
+
+        btn = self.qtui.findChild(QPushButton, 'button_exportselected')
+        #btn.clicked.connect()
+
+
+    def closeEvent(self, event):
+        self.close()
+
         
-        ###Create Project Directory Options
-        pm.rowColumnLayout( "mayaPath", numberOfColumns=3, columnWidth = [(1,300), (2,50), (3,300)])
-        pm.text(al = "center", label = "Skin Skeleton Hierarchy")
-        pm.text(label = "")
-        pm.text(al = "center", label = "Rig Skeleton Hierarchy")
-        pm.textScrollList( "Skin_Skel_List", ams=False, w = 300, h = 400, append = self.FindBaseJoint())
-        pm.text(label = "")
-        pm.textScrollList( "Rig_Skel_List", ams=False, w = 300, h = 400)
+def main():
+    global ui_win 
+    #ui_win = MayaUI.MQtUtil.findWindow('Consensys Animation Export')
 
-        mc.setParent("..")
+    try:
+        ui_win.close()
+        print 'refresh'
+    except:
+        print 'pass'
 
-        pm.rowColumnLayout("buttonLayout", numberOfColumns=3, columnWidth = [(1,100), (2,350), (3,100)])
-        pm.text(label="")
-        pm.button(al = "center", label = "Move References to local folders", command = lambda *_:self.StartExport())
-        pm.text(label="")
+    ui_win = window_ui()
+    ui_win.setObjectName('ExportUI')
+    ui_win.show()
+    return ui_win
 
-        mc.showWindow()
+    
+    
+    
+#mc.GamePipeline(sp = "Unity", sup = True)
+#setAttr("gameExporterPreset4.animClips[1].animClipStart") 25;                                
+#gameExp_AdjustTimeline("1", "StartClip", 16);
+#workspaceControl -q -floating CharcoalEditor2Window;
+#workspaceControl -q -floating CharcoalEditor2Window;
+#setAttr("gameExporterPreset4.animClips[0].animClipStart") 0;                                
+#gameExp_AdjustTimeline("0", "StartClip", 16);
+#workspaceControl -q -floating CharcoalEditor2Window;
+#workspaceControl -q -floating CharcoalEditor2Window;
+
+class UI_Functionality():
+    
+    def __init__():
+        print 'launching export window'
         
+    def onExitCode(self):
+        """Do this when the script is closed"""
+        sys.stdout.write("You closed the demo ui!\n")
         
     #Open up export UI
 	#List available rigs in the scene (each rig can have a specific node that can be searched for, e.g. PipelineExport = True)
@@ -107,11 +116,4 @@ class Initialize(object):
         print 'removed animation rig'
 
     def FindBaseJoint(self):
-        print 'found the base jump'
         return 'nothing found'
-        
-
-class LaunchWindow():
-    
-    def __init__():
-        print 'launching export window'
