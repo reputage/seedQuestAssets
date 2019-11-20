884c0e429a52bffd35ab8a4f2b27e8e3097fe702
AndyWinkler <winkanimation@gmail.com>
2019-11-20 05:24:50 -0700

    Added skin swapper to project. Add skinnedmeshswapper to parent object of rig.

--- seedQuestAssets/Assets/SeedQuestAssets/Scripts/SkinnedMeshSwapper.cs
+++ seedQuestAssets/Assets/SeedQuestAssets/Scripts/SkinnedMeshSwapper.cs
@@ -1,77 +1,56 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SkinnedMeshSwapper : MonoBehaviour {
 
-    public GameObject jointHierarchy;
-    public GameObject meshParent;
-    public SkinnedMeshRenderer oldMesh;
-    public SkinnedMeshRenderer newMesh;
-    public bool swapped = false;
+//How to use:
+/*
+ * Attach this script to the parent object of a rig.
+ * Script automatically finds the Joint_Grp object and Mesh_Grp child object.
+ * Drag the other character that you want to swap meshes with into the scene.
+ * Pick and choose the skinned mesh renderers that you want to swap between and click swap skins.
+ * Save swapped character as a new prefab. Skinning should work if using the same rig.
+*/
 
 
-    public void Update()
+public class SkinnedMeshSwapper : MonoBehaviour
+{
+    [HideInInspector] public GameObject jointHierarchy;
+    [HideInInspector] public GameObject meshParent;
+    [HideInInspector] public SkinnedMeshRenderer oldMesh;
+    [HideInInspector] public SkinnedMeshRenderer newMesh;
+    [HideInInspector] public bool swapped = false;
+    [HideInInspector] string parent;
+
+    public void Awake()
     {
-
-        if (Input.GetKeyDown("space"))
+        if (parent == null)
         {
-            print("Get Key pressed");
-
-            if (swapped == false)
-            {
-                ProcessBonedObject(newMesh, oldMesh, jointHierarchy);
-            }
-            else
-            {
-                print("already swapped");
-            }
-
+            parent = gameObject.name;
         }
-
-    }
-
-    private void ProcessBonedObject(SkinnedMeshRenderer newMesh, SkinnedMeshRenderer oldMesh, GameObject RootObj)
-    {
-        /*      Create the SubObject        */
-        GameObject NewObj = new GameObject(newMesh.gameObject.name);
-        NewObj.transform.parent = meshParent.transform;
-
-        /*      Add the renderer        */
-        NewObj.AddComponent<SkinnedMeshRenderer>();
-        SkinnedMeshRenderer NewRenderer = NewObj.GetComponent<SkinnedMeshRenderer>();
-
-        /*      Assemble Bone Structure     */
-        Transform[] MyBones = new Transform[newMesh.bones.Length];
-        for (var i = 0; i < newMesh.bones.Length; i++)
-            MyBones[i] = FindChildByName(newMesh.bones[i].name, RootObj.transform);
-
-        /*      Assemble Renderer       */
-        NewRenderer.bones = MyBones;
-        NewRenderer.sharedMesh = newMesh.sharedMesh;
-        NewRenderer.materials = newMesh.materials;
-
-        /*      Set Parent Joint        */
-        newMesh.GetComponent<SkinnedMeshRenderer>().rootBone = oldMesh.GetComponent<SkinnedMeshRenderer>().rootBone;
-
-        /*      Disable Old Mesh        */
-        //oldMesh.GetComponent<Renderer>().enabled = false;
-
-        //swapped = true;
-    }
-
-    private Transform FindChildByName(string newMeshBones, Transform rootTransform)
-    {
-        Transform ReturnObj;
-        if(rootTransform.name==newMeshBones )
-            return rootTransform.transform;
-
-        foreach (Transform child in rootTransform )
+        else
         {
-            ReturnObj = FindChildByName(newMeshBones, child );
-            if(ReturnObj )
-                return ReturnObj;
+            if (jointHierarchy == null)
+            {
+                jointHierarchy = GameObject.Find(parent + "/Joint_Grp");
+
+                if (jointHierarchy == null)
+                {
+                    print("unable to find mesh");
+                }
+            }
+
+            if (meshParent == null)
+            {
+                string name = gameObject.name;
+                GameObject parent = GameObject.Find(name + "/Mesh_Grp/");
+
+                meshParent = parent.transform.GetChild(0).gameObject;
+
+                if (meshParent == null)
+                {
+                    print("unable to find mesh");
+                }
+            }
         }
-        return null;
     }
 }
---
+++ seedQuestAssets/Assets/SeedQuestAssets/Scripts/SwappableBodyPartsEditor.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+[CustomEditor(typeof(SkinnedMeshSwapper))]
+[CanEditMultipleObjects]
+public class SwappableBodyPartsEditor : Editor
+{
+    SerializedProperty jointHierarchy;
+    SerializedProperty meshParent;
+    SerializedProperty oldMesh;
+    SerializedProperty newMesh;
+
+
+    void OnEnable()
+    {
+        // Setup the SerializedProperties.
+        jointHierarchy = serializedObject.FindProperty("jointHierarchy");
+        meshParent = serializedObject.FindProperty("meshParent");
+        oldMesh = serializedObject.FindProperty("oldMesh");
+        newMesh = serializedObject.FindProperty("newMesh");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        SkinnedMeshSwapper skinswap = (SkinnedMeshSwapper)target;
+
+        skinswap.Awake();
+
+        serializedObject.Update();
+        DrawDefaultInspector();
+
+
+        bool allowSceneObjects = !EditorUtility.IsPersistent(target);
+
+        skinswap.jointHierarchy = (GameObject)EditorGUILayout.ObjectField("joint hierarchy", skinswap.jointHierarchy, typeof(GameObject), allowSceneObjects);
+        skinswap.meshParent = (GameObject)EditorGUILayout.ObjectField("mesh Parent", skinswap.meshParent, typeof(GameObject), allowSceneObjects);
+        skinswap.oldMesh = (SkinnedMeshRenderer)EditorGUILayout.ObjectField("old Mesh", skinswap.oldMesh, typeof(SkinnedMeshRenderer), allowSceneObjects);
+        skinswap.newMesh = (SkinnedMeshRenderer)EditorGUILayout.ObjectField("new Mesh", skinswap.newMesh, typeof(SkinnedMeshRenderer), allowSceneObjects);
+
+        if (GUILayout.Button("Swap Skins"))
+        {
+            ProcessBonedObject(skinswap.newMesh, skinswap.oldMesh, skinswap.jointHierarchy, skinswap.meshParent);
+        }
+
+    }
+
+    public void ProcessBonedObject(SkinnedMeshRenderer newMesh, SkinnedMeshRenderer oldMesh, GameObject RootObj, GameObject meshParent)
+    {
+        /*      Create the SubObject        */
+        GameObject NewObj = new GameObject(newMesh.gameObject.name);
+        NewObj.transform.parent = meshParent.transform;
+
+        /*      Add the renderer        */
+        NewObj.AddComponent<SkinnedMeshRenderer>();
+        SkinnedMeshRenderer NewRenderer = NewObj.GetComponent<SkinnedMeshRenderer>();
+
+        /*      Assemble Bone Structure     */
+        Transform[] MyBones = new Transform[newMesh.bones.Length];
+        for (var i = 0; i < newMesh.bones.Length; i++)
+            MyBones[i] = FindChildByName(newMesh.bones[i].name, RootObj.transform);
+
+        /*      Assemble Renderer       */
+        NewRenderer.bones = MyBones;
+        NewRenderer.sharedMesh = newMesh.sharedMesh;
+        NewRenderer.materials = newMesh.materials;
+
+        /*      Set Parent Joint        */
+        newMesh.GetComponent<SkinnedMeshRenderer>().rootBone = oldMesh.GetComponent<SkinnedMeshRenderer>().rootBone;
+
+        /*      Disable Old Mesh        */
+        oldMesh.gameObject.SetActive(false);
+
+        //swapped = true;
+    }
+
+    public Transform FindChildByName(string newMeshBones, Transform rootTransform)
+    {
+        Transform ReturnObj;
+        if (rootTransform.name == newMeshBones)
+            return rootTransform.transform;
+
+        foreach (Transform child in rootTransform)
+        {
+            ReturnObj = FindChildByName(newMeshBones, child);
+            if (ReturnObj)
+                return ReturnObj;
+        }
+        return null;
+    }
+}
