8e57245885da6d904348d31c1604c8d2bc0db3bf
AndyWinkler <winkanimation@gmail.com>
2019-11-21 12:38:12 -0700

    Reorganized classes to fix reference issue

--- WorkingArtFolder/_Animation/maya/scripts/Skin_Animation_Export.py
+++ WorkingArtFolder/_Animation/maya/scripts/Skin_Animation_Export.py
@@ -19,7 +19,95 @@ def get_main_window():
     except:
         mayaMainWindow = None
     return mayaMainWindow
+
+class UI_Functionality(object):
     
+    def __init__(self):
+        print 'initialize functionality'
+        
+    #Open up export UI
+	#List available rigs in the scene (each rig can have a specific node that can be searched for, e.g. PipelineExport = True)
+    def FindAllRigs(self):
+        sceneRigs = []
+        selection = mc.ls(l = True)
+        for obj in selection:
+            if mc.nodeType(obj) == 'container':
+                sceneRigs.append(obj)
+
+        return sceneRigs
+        #search for all assets in a scene that have this property: 'ConsensysAnimRig'
+        #determine if the rig is a reference or imported into scene
+            #if already imported, do nothing
+            #else, place import button next to rig
+    
+    def FindRigDetails(self):
+        #query list of containers from UI
+        items = self.FindAllRigs()
+        referenced = []  
+        print items
+        for x in items:
+            if mc.referenceQuery(x, isNodeReferenced=True):
+                referenced.append("R")
+            else:
+                referenced.append(" ")
+        print items
+        return referenced
+        
+    def FindAnimationClipsinScene(self):
+        containers = self.FindAllRigs()
+        animations = []
+        #get current container
+        for x in containers:
+            temp = mc.container(x,q = True, nl = True)
+            
+            for u in temp:
+                animations.append(self.FillSequenceUI(u, ".AnimationName"))
+            break
+
+        return animations
+        
+    def UpdateAnimationList(self, selection):
+        print 'finding animation clips...'
+        animations = []
+        print selection
+        if len(selection) == 0:
+            return ['']
+        else:
+            #get current container
+            for x in selection:
+                temp = mc.container(x,q = True, nl = True)
+                
+                for u in temp:
+                    animations.append(self.FillSequenceUI(u, ".AnimationName"))
+                    
+            return animations
+ 
+    def FillSequenceUI(self, seq, attr):
+        info = mc.getAttr(seq + attr)
+        return info   
+        
+    def SelectionChanged(self):
+        print 'changed'
+	#Each rig can have a node that manages the animation frame data (Walk 1-55, etc etc)
+    #Write a separate script or have a window that can display and manage this information
+	#Export should be able to export fbx's that support automatically splitting the exported animations into clips
+	#Have option to fix FBX export settings
+    #Ability to export skinned asset meshes that do not have animation
+    def StartExport(self):
+        print 'Started Export!'
+        
+    def BakeSkinJoints():
+        print 'baked skin joints'
+
+    def CleanNamespaces():
+        print 'clean namespace'
+        
+    def RemoveAnimRig(self):
+        print 'removed animation rig'
+
+    def FindBaseJoint(self):
+        return 'nothing found'
+        
 class window_ui(QDialog):
     #class instance
     global ui_funct
@@ -39,7 +127,6 @@ class window_ui(QDialog):
         self.setWindowFlags(Qt.Window)
         self.setWindowTitle('Consensys Animation Export')
         self.setObjectName('ExportUI')
-        #self.setWindowFlags(Qt.Window)
         self.setContentsMargins(5, 5, 5, 5)
         self.main_layout = QVBoxLayout()
         self.main_layout.setContentsMargins(0, 0, 0, 0)
@@ -48,23 +135,15 @@ class window_ui(QDialog):
         self.main_layout.addWidget(self.qtui)
 
         btn = self.qtui.findChild(QPushButton, 'button_exportselected')
-        #btn.clicked.connect()
-        
-        #class instance
-        #ui_funct = UI_Functionality()
+
         #fill out rig list
         self.qtui.list_scenerigs.addItems(ui_funct.FindAllRigs())
         #fill out rig ref list
         self.qtui.list_scenerigsreference.addItems(ui_funct.FindRigDetails())
-
         #fill animation scene rigs
         self.qtui.list_animations.addItems(ui_funct.FindAnimationClipsinScene())
-        
+        #update animation list based on rig selection
         self.qtui.list_scenerigs.itemSelectionChanged.connect(self.UpdateAnimList)
-        #callbacks from selection change
-        #idx = qtui.addEventCallback("SelectionChanged", test)
-        #when ever you finish doing your stuff
-        #OpenMaya.MMessage.removeCallback(idx)
 
 
     def UpdateAnimList(self):
@@ -102,96 +181,3 @@ def main():
 #gameExp_AdjustTimeline("0", "StartClip", 16);
 #workspaceControl -q -floating CharcoalEditor2Window;
 #workspaceControl -q -floating CharcoalEditor2Window;
-
-class UI_Functionality():
-    
-    def __init__(self):
-        print 'initialize functionality'
-        
-    #Open up export UI
-	#List available rigs in the scene (each rig can have a specific node that can be searched for, e.g. PipelineExport = True)
-    def FindAllRigs(self):
-        sceneRigs = []
-        selection = mc.ls(l = True)
-        for obj in selection:
-            if mc.nodeType(obj) == 'container':
-                sceneRigs.append(obj)
-
-        return sceneRigs
-        #search for all assets in a scene that have this property: 'ConsensysAnimRig'
-        #determine if the rig is a reference or imported into scene
-            #if already imported, do nothing
-            #else, place import button next to rig
-
-        
-    def FindRigDetails(self):
-        #query list of containers from UI
-        items = self.FindAllRigs()
-        referenced = []  
-        print items
-        for x in items:
-            if mc.referenceQuery(x, isNodeReferenced=True):
-                referenced.append("R")
-            else:
-                referenced.append(" ")
-        print items
-        return referenced
-
-        
-    def FindAnimationClipsinScene(self):
-        containers = self.FindAllRigs()
-        animations = []
-        #get current container
-        for x in containers:
-            temp = mc.container(x,q = True, nl = True)
-            
-            for u in temp:
-                animations.append(self.FillSequenceUI(u, ".AnimationName"))
-            break
-
-        return animations
-        
-    def UpdateAnimationList(self, selection):
-        print 'finding animation clips...'
-        animations = []
-        print selection
-        if len(selection) == 0:
-            return ['']
-        else:
-            #get current container
-            for x in selection:
-                temp = mc.container(x,q = True, nl = True)
-                
-                for u in temp:
-                    animations.append(self.FillSequenceUI(u, ".AnimationName"))
-            print "Animations:    "
-            print animations
-            return animations
-            
-        
-        
-    def FillSequenceUI(self, seq, attr):
-        info = mc.getAttr(seq + attr)
-        return info   
-        
-    def SelectionChanged(self):
-        print 'changed'
-	#Each rig can have a node that manages the animation frame data (Walk 1-55, etc etc)
-    #Write a separate script or have a window that can display and manage this information
-	#Export should be able to export fbx's that support automatically splitting the exported animations into clips
-	#Have option to fix FBX export settings
-    #Ability to export skinned asset meshes that do not have animation
-    def StartExport(self):
-        print 'Started Export!'
-        
-    def BakeSkinJoints():
-        print 'baked skin joints'
-
-    def CleanNamespaces():
-        print 'clean namespace'
-        
-    def RemoveAnimRig(self):
-        print 'removed animation rig'
-
-    def FindBaseJoint(self):
-        return 'nothing found'
