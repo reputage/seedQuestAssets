c42c10a9d325d04885ad95fd12c0355c8e8363de
Brady Hammond <bradymh23@gmail.com>
2019-12-04 01:35:38 -0700

    Start Connecting Mobile Version

---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Mobile/MobileBottomMenu.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MobileBottomMenu : MonoBehaviour
+{
+    static private MobileBottomMenu instance = null;
+    static private MobileBottomMenu setInstance() { instance = GameObject.FindObjectOfType<MobileBottomMenu>(); return instance; }
+    static public MobileBottomMenu Instance { get { return instance == null ? setInstance() : instance; } }
+
+    public Sprite buttonSprite;
+    public Sprite alternateButtonSprite;
+
+    private Animator animator;
+    private bool mobileMenuUp;
+    private Image[] images;
+    private bool inGame;
+    private bool mainMenu;
+
+    static public bool InGame
+    {
+        get { return Instance.inGame; }
+        set { Instance.inGame = value; }
+    }
+
+    static public bool MainMenu
+    {
+        get { return Instance.mainMenu; }
+        set { Instance.mainMenu = value; }
+    }
+
+    void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "MobileStartMenu")
+            inGame = false;
+        animator = gameObject.GetComponentInChildren<Animator>();
+        images = animator.gameObject.transform.GetComponentsInChildren<Image>(true);
+        mobileMenuUp = false;
+    }
+
+    public void OnClickMobileMenu()
+    {
+        if (mainMenu)
+        {
+            images[2].color = new Color(0.90588f, 0.91765f, 0.92157f);
+            images[3].color = new Color(0.13333f, 0.35294f, 0.50196f);
+            images[12].sprite = alternateButtonSprite;
+
+            images[10].gameObject.SetActive(false);
+        }
+
+        else
+        {
+            images[2].color = new Color(1f, 1f, 1f);
+            images[3].color = new Color(0.227451f, 0.5921569f, 0.8392157f);
+            images[12].sprite = buttonSprite;
+            images[10].gameObject.SetActive(true);
+        }
+
+        if (!inGame)
+            images[4].gameObject.SetActive(false);
+        else
+            images[4].gameObject.SetActive(true);
+
+
+        if (!mobileMenuUp)
+        {
+            animator.Play("StartMenuMobileUp");
+            images[0].gameObject.SetActive(false);
+            images[13].gameObject.SetActive(true);
+            images[12].transform.GetComponentInChildren<TMPro.TMP_Text>(true).gameObject.SetActive(true);
+            mobileMenuUp = !mobileMenuUp;
+        }
+
+
+        else
+        {
+            animator.Play("StartMenuMobileDown");
+            images[0].gameObject.SetActive(true);
+            images[13].gameObject.SetActive(false);
+            images[12].transform.parent.GetComponentInChildren<TMPro.TMP_Text>(true).gameObject.SetActive(false);
+            mobileMenuUp = !mobileMenuUp;
+        }
+    }
+
+    public void ResetBottomMenu()
+    {
+        animator.Play("StartMenuMobileHidden");
+        images[0].gameObject.SetActive(true);
+        images[13].gameObject.SetActive(false);
+        images[12].transform.parent.GetComponentInChildren<TMPro.TMP_Text>(true).gameObject.SetActive(false);
+        mobileMenuUp = false;
+    }
+}
+
---
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Mobile/MobileMenuScreens.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Runtime.InteropServices;
+
+using SeedQuest.Interactables;
+
+public class MobileMenuScreens : MonoBehaviour
+{
+    static private MobileMenuScreens instance = null;
+    static private MobileMenuScreens setInstance() { instance = GameObject.FindObjectOfType<MobileMenuScreens>(); return instance; }
+    static public MobileMenuScreens Instance { get { return instance == null ? setInstance() : instance; } }
+
+    public MenuScreenStates state = MenuScreenStates.Start;
+
+    private Canvas[] canvas;
+    private Canvas bottomMenuCanvas;
+    private Canvas startCanvas;
+    private Canvas encodeCanvas;
+    private Canvas sceneSelectionCanvas;
+    private Canvas sceneLineUpCanvas;
+    private Canvas actionLineUpCanvas;
+    private Canvas helpCanvas;
+    private Canvas settingsCanvas;
+
+    private Canvas previousCanvas;
+
+    void Awake()
+    {
+        SetComponentReferences();
+    }
+
+    private void Start()
+    {
+        GameManager.State = GameState.Menu;
+        GoToStart();
+    }
+
+    private void SetComponentReferences()
+    {
+        canvas = GetComponentsInChildren<Canvas>(true);
+        startCanvas = canvas[1];
+        encodeCanvas = canvas[2];
+        sceneSelectionCanvas = canvas[3];
+        sceneLineUpCanvas = canvas[4];
+        actionLineUpCanvas = canvas[5];
+        settingsCanvas = canvas[6];
+        helpCanvas = canvas[7];
+        bottomMenuCanvas = canvas[8];
+    }
+
+    public void ResetCanvas()
+    {
+        foreach (Canvas _canvas in canvas)
+        {
+            if (_canvas != canvas[0])
+                _canvas.gameObject.SetActive(false);
+        }
+    }
+
+    public void ActiveCanvas()
+    {
+        foreach (Canvas _canvas in canvas)
+        {
+            if (_canvas != canvas[0] && _canvas != canvas[8])
+            { 
+                if (_canvas.gameObject.activeSelf)
+                {
+                    previousCanvas = _canvas;
+                    return;
+                }
+            }
+        }
+
+        previousCanvas = startCanvas;
+    }
+
+    public void GoToStart()
+    {
+        ActiveCanvas();
+        InteractablePathManager.Reset();
+        MobileBottomMenu.MainMenu = true;
+        state = MenuScreenStates.Start;
+        ResetCanvas();
+        MobileBottomMenu.Instance.ResetBottomMenu();
+        startCanvas.gameObject.SetActive(true);
+        bottomMenuCanvas.gameObject.SetActive(true);
+    }
+
+    public void GoToHelp()
+    {
+        ActiveCanvas();
+        MobileBottomMenu.MainMenu = false;
+        ResetCanvas();
+        MobileBottomMenu.Instance.ResetBottomMenu();
+        helpCanvas.gameObject.SetActive(true);
+        bottomMenuCanvas.gameObject.SetActive(true);
+    }
+
+    public void GoToSettings()
+    {
+        ActiveCanvas();
+        MobileBottomMenu.MainMenu = false;
+        ResetCanvas();
+        MobileBottomMenu.Instance.ResetBottomMenu();
+        settingsCanvas.gameObject.SetActive(true);
+        bottomMenuCanvas.gameObject.SetActive(true);
+    }
+
+    public void SetModeLearnSeed()
+    {
+        GameManager.Mode = GameMode.Rehearsal;
+        GoToEncode();
+    }
+
+    public void SetModeRecoverSeed()
+    {
+        GameManager.Mode = GameMode.Recall;
+        GoToSceneSelection();
+    }
+
+    public void GoToEncode()
+    {
+        ActiveCanvas();
+        state = MenuScreenStates.SeedSetup;
+        ResetCanvas();
+        encodeCanvas.gameObject.SetActive(true);
+    }
+
+    public void GoToSceneSelection()
+    {
+        ActiveCanvas();
+        state = MenuScreenStates.EncodeSeed;
+        ResetCanvas();
+        sceneSelectionCanvas.gameObject.SetActive(true);
+        sceneSelectionCanvas.gameObject.GetComponent<EncodeSeedCanvas>().resetCanvas();
+        sceneSelectionCanvas.gameObject.GetComponent<EncodeSeedCanvas>().resetSeedStr();
+        LevelSetManager.ResetCurrentLevels();
+        WorldManager.Reset();
+    }
+
+    public void GoToSceneLineUp()
+    {
+        ActiveCanvas();
+        GameManager.State = GameState.Menu;
+        state = MenuScreenStates.SceneLineUp;
+        ResetCanvas();
+        sceneLineUpCanvas.gameObject.SetActive(true);
+    }
+
+    public void ReturnToSceneLineUp()
+    {
+        ActiveCanvas();
+        GoToSceneLineUp();
+    }
+
+    public void GoToActionLineUp()
+    {
+        ActiveCanvas();
+        GameManager.State = GameState.Menu;
+        if (GameManager.Mode == GameMode.Rehearsal)
+        {
+            state = MenuScreenStates.ActionLineUp;
+            ResetCanvas();
+            actionLineUpCanvas.gameObject.SetActive(true);
+        }
+        else
+        {
+            CloseSceneLineUp();
+        }
+    }
+
+    public void CloseSceneLineUp()
+    {
+        CameraZoom.StartZoomIn();
+        ResetCanvas();
+        GameManager.State = GameState.Play;
+    }
+
+    public void Back()
+    {
+        Canvas temp = previousCanvas;
+        if (temp == startCanvas)
+            MobileBottomMenu.MainMenu = true;
+        else
+            MobileBottomMenu.MainMenu = false;
+        ActiveCanvas();
+        ResetCanvas();
+        MobileBottomMenu.Instance.ResetBottomMenu();
+        temp.gameObject.SetActive(true);
+        bottomMenuCanvas.gameObject.SetActive(true);
+
+        if ( previousCanvas == helpCanvas || previousCanvas == settingsCanvas)
+        {
+            for (int i = 0; i < canvas.Length; i++)
+            {
+                if (temp == canvas[i] && i > 1)
+                {
+                    previousCanvas = canvas[i - 1];
+                }
+            }
+        }
+
+        if (temp == encodeCanvas)
+        {
+            previousCanvas = startCanvas;
+        }
+    }
+}
