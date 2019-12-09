6cdb0e458bf96c58c8fc55d50ebb8ef4d836cca1
Michael Mendoza <m.mendoza711@gmail.com>
2019-12-09 14:54:36 +0000

    Setup for recall experiment

---
+++ seedQuestPrototypes/Assets/SeedQuestPrototype/Scripts/ExperimentalDemoManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperimentalDemoManager : MonoBehaviour
+{
+    static public Dictionary<string, int> users; 
+
+    void Start() {
+        users = new Dictionary<string, int>();
+        users.Add("user00", 0);
+        users.Add("user01", 1);
+        users.Add("user02", 2);
+        users.Add("user03", 3);
+        users.Add("user04", 4);
+
+        users.Add("dannyzuckerman", 1);
+        users.Add("georgegogan", 2);
+        users.Add("danielaosorio", 3);
+        users.Add("thessymehrain", 4);
+    }
+
+}
