84f246d7bb45186e2ead5b0ec7a52519014c611d
Michael Mendoza <askmichaelmendoza@gmail.com>
2020-04-02 22:59:03 -0400

    Moved MonoWaitBehavior class into it's own file.

--- seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/MenuScreenV2.cs
+++ seedQuestPrototypes/Assets/SeedQuestSandbox/Scripts/Menu/MenuScreenV2.cs
@@ -4,28 +4,6 @@ using UnityEngine;
 
 using SeedQuest.Interactables;
 
-public class MonoWaitBehavior : MonoBehaviour {
-    /// <summary>
-    /// Runs an Action method after waiting the input number of seconds 
-    /// </summary>
-    /// <param name="seconds"> Seconds to wait </param>
-    /// <param name="action"> Action method to run after waiting </param>
-    public void Wait(float seconds, Action action) {
-        StartCoroutine(_wait(seconds, action));
-    }
-
-    /// <summary>
-    /// Helper function to run an Action method after waiting the input number of seconds 
-    /// </summary>
-    /// <param name="time"> Time to wait (seconds) </param>
-    /// <param name="callback"> Action method to run after waiting </param>
-    /// <returns></returns>
-    IEnumerator _wait(float time, Action callback) {
-        yield return new WaitForSeconds(time);
-        callback();
-    }
-}
-
 public class MenuScreenV2 : MonoWaitBehavior
 {
     static private MenuScreenV2 instance = null;
